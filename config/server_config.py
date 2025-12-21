2d391e50cfbf9ee04916d88a7b89edd2c6656491
ngochung1008 <phamngochung02122005@gmail.com>
2025-12-22 01:08:27 +0700

    fix 1

--- config/server_config.py
+++ config/server_config.py
@@ -1,7 +1,7 @@
 # ===== CẤU HÌNH QUAN TRỌNG =====
 # Thay "192.168.2.31" bằng IP thực của máy chạy server
 # Hoặc dùng "localhost" nếu tất cả chạy trên cùng 1 máy
-SERVER_IP = "192.168.5.142"  # Địa chỉ IP của máy server
+SERVER_IP = "192.168.2.193"  # Địa chỉ IP của máy server
 SERVER_HOST = 5000         # Port cho main server (video streaming)
 AUTH_HOST = 5001           # Port cho auth server (đăng nhập)
 CLIENT_PORT = 5000         # Port client kết nối tới (phải trùng SERVER_HOST)
--- src/manager/manager.py
+++ src/manager/manager.py
@@ -263,7 +263,7 @@ class Manager(QObject):
 
 if __name__ == "__main__":
     # 1. Cấu hình
-    HOST = "192.168.5.142"
+    HOST = "192.168.2.193"
     PORT = 5000
     MANAGER_ID = "manager_gui_1"
 
